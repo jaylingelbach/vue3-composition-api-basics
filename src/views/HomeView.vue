69bfa708170acce03b9bb9393294fd47cfb7eafc
Jay <jaylingelbach@users.noreply.github.com>
2022-07-26 15:46:43 -0500

    basic options counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,38 @@
 <template>
 <div class="home"> 
-  <h1>Home</h1>
+  <div>
+    <button @click="decreaseCounter" class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button @click="increaseCounter" class="btn">+</button>
+  </div>
 </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    }
+  }
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
